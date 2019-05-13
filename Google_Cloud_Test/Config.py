731e75b0b2829914db6a206e9811ddb3c4f32908
jnirwin1 <josh@teleoinc.com>
2019-05-13 14:37:53 -0600

    Added Database Integration

---
+++ Google_Cloud_Test/Config.py
@@ -0,0 +1,7 @@
+# Config.py:
+
+HMI_IP = "192.168.1.35"
+
+
+Node_Tags = ["PLC//PLC_TIME/DT_Now"]
+
---
+++ Google_Cloud_Test/db_time_client.py
@@ -0,0 +1,44 @@
+from mysql.connector import MySQLConnection, Error
+from dbconfig import read_db_config
+from datetime import datetime, timedelta
+from opcua import Client
+from opcua import ua
+from Config import *
+
+
+def insert_time(time):
+    query = ("INSERT INTO plc_time(time) "
+             "VALUES(%s)")
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, time.strftime('%Y-%m-%d %H:%M:%S'))
+
+        conn.commit()
+    except Error as error:
+        print(error)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    client = Client("opc.tcp://admin:admin@"+str(HMI_IP)+":48010/")
+     
+    try:
+        client.connect()
+
+        nodes = [client.get_node("ns=2;s=Tags."+ tag) for tag in Node_Tags]
+       
+
+        while True:
+            dataRow = [node.get_value for node in nodes]
+            time = datetime.fromtimestamp(dataRow[0].get_value())
+            insert_time(time)
+            time.sleep(1)
+
+    finally:
+        client.disconnect()
